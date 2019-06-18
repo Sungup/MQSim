6a9f1e6e7ac3a1519e4a582b777e987b4042da33
Sungup <sungup@me.com>
2019-06-18 21:29:09 +0900

    [update] update script files for argument parsing

--- run-test.sh
+++ run-test.sh
@@ -11,39 +11,42 @@ SAMPLE_CONFIG_HOME=${SAMPLE_HOME}/config
 SAMPLE_BASELINE_HOME=${SAMPLE_HOME}/baseline
 SAMPLE_DIFFERENTIAL=${SAMPLE_HOME}/differential.diff
 
-# Get number of threads for compile
-if [[ "Darwin" == $(uname) ]]; then
-  # macOS commands
-  __N_CORES=$(sysctl -n hw.logicalcpu)
-else
-  # linux commands
-  __N_CORES=$(nproc)
-fi
+# ==================
+# Internal functions
+# ==================
+function __build_mqsim() {
+  # Get number of threads for compile
+  if [[ "Darwin" == $(uname) ]]; then
+    # macOS commands
+    __N_CORES=$(sysctl -n hw.logicalcpu)
+  else
+    # linux commands
+    __N_CORES=$(nproc)
+  fi
 
-# ====================================
-# Make build directory and build MQSim
-# ====================================
-export CC=clang
-export CXX=clang++
+  if [[ -d ${PROJECT_BUILD_HOME} ]]; then
+    rm -Rf ${PROJECT_BUILD_HOME};
+  fi
 
-if [[ -d ${PROJECT_BUILD_HOME} ]]; then
-  rm -Rf ${PROJECT_BUILD_HOME};
-fi
+  mkdir ${PROJECT_BUILD_HOME};
 
-mkdir ${PROJECT_BUILD_HOME};
+  cd ${PROJECT_BUILD_HOME};
 
-cd ${PROJECT_BUILD_HOME};
+  # Build
+  cmake -DCMAKE_BUILD_TYPE=Release -DFORCE_INLINE=ON ${PROJECT_HOME};
+  make -j ${__N_CORES};
 
-# Build
-cmake -DCMAKE_BUILD_TYPE=Release -DFORCE_INLINE=ON ${PROJECT_HOME};
-make -j ${__N_CORES};
+  # Return to project home
+  cd ${PROJECT_HOME}
+}
 
-# Return to project home
-cd ${PROJECT_HOME}
+function __kill_all_other_mqsim() {
+  # Before run, kill all MQSim.
+  if [[ 0 -lt $(ps -e | grep MQSim | wc -l) ]]; then
+    killall MQSim;
+  fi
+}
 
-# =============================
-# Run simulator for all samples
-# =============================
 function __run_sample() {
   __SAMPLE_HOME=$2/$1
   __RESULT_HOME=$3/$1
@@ -55,17 +58,20 @@ function __run_sample() {
   rm ${__SAMPLE_HOME}/*_scenario_*.xml 2> /dev/null;
 
   for __FLOW in ${__SAMPLE_HOME}/flow-[0-9]*.xml; do
-    # Hide output
-    echo - Run: build/bin/MQSim -i ${__SAMPLE_SSD_CONF} -w ${__FLOW};
-    build/bin/MQSim -i ${__SAMPLE_SSD_CONF} -w ${__FLOW} > /dev/null;
+    echo "[run]     ${PROJECT_BUILD_HOME}/bin/MQSim -i ${__SAMPLE_SSD_CONF} -w ${__FLOW}";
+    ${PROJECT_BUILD_HOME}/bin/MQSim -i ${__SAMPLE_SSD_CONF} -w ${__FLOW} > /dev/null;
   done;
 
   mkdir -p ${__RESULT_HOME};
   mv ${__SAMPLE_HOME}/*_scenario_*.xml ${__RESULT_HOME};
 }
 
-# Before run kill all MQSim
-killall MQSim;
+# =============================
+# Run simulator for all samples
+# =============================
+__kill_all_other_mqsim;
+
+__build_mqsim;
 
 for __DIR in ${SAMPLE_CONFIG_HOME}/*; do
   __run_sample "$(basename ${__DIR})"  \
@@ -73,4 +79,13 @@ for __DIR in ${SAMPLE_CONFIG_HOME}/*; do
                "${SAMPLE_RESULT_HOME}";
 done
 
+# Check result differentials.
 diff -urN ${SAMPLE_BASELINE_HOME} ${SAMPLE_RESULT_HOME} > ${SAMPLE_DIFFERENTIAL};
+
+if [[ 0 -lt $(cat ${SAMPLE_DIFFERENTIAL} | wc -l) ]]; then
+  echo "[failed]  Different simulation result!" 1>&2;
+  exit -1;
+else
+  echo "[success] Same simulation result with before";
+  exit 0;
+fi
